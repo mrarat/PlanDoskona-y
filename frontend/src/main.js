df32f0ee50b04995de323ccc1c2f72e73389097d
LarkyAngel <59319622+LarkyAngel@users.noreply.github.com>
2023-05-04 23:55:08 +0200

    added algorithm

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,19 @@ import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import Multiselect from 'vue-multiselect'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+const app = createApp(App).component('multiselect', Multiselect)
+app.use(vuetify)
+
+app.mount('#app')
