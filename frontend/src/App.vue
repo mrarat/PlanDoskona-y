937ee9b82b9a4194dd49ff3f0084b4c959a49683
mrarat <46651686+mrarat@users.noreply.github.com>
2023-05-04 23:21:19 +0200

    api querries still do not work

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,8 +6,10 @@ const USOS_API_URL = 'https://usosapps.uw.edu.pl/services'
 const BACKEND_API_URL =
   import.meta.env.VITE_API_URL == undefined ? 'http://localhost:8000' : import.meta.env.VITE_API_URL
 
-  const drawer = ref(null)
+// Drawer
+const drawer = ref(null)
 
+// Select semester
 const select_semester = ref(null);
 const items_semester = ref([
   { title: 'Semestr letni 2023/2024 (2023L)', value: '2023L' },
@@ -18,28 +20,16 @@ const items_semester = ref([
   { title: 'Semestr zimowy 2021 (2021Z)', value: '2021Z' },
 ]);
 
+// Select course
+const loading_course = ref(false)
+const search_course = ref(null)
 const select_course = ref(null);
-const items_course = ref([{ state: 'some state', abbr: 'some abbr' },]);
+const items_course = ref([]);
 
-const search1 = async (query) => {
-  if (!query) return;
-  
-  // Simulate a 400ms delay
-  await new Promise((resolve) => setTimeout(resolve, 400));
+async function query_course_search(course_name) {
+  loading_course.value = true
 
-  // Assign the simulated data to items1
-  items_course.value = [
-    { state: 'some state', abbr: 'some abbr' },
-  ];
-};
-
-const loading_test = ref(false)
-const search_test = ref(null)
-const select_test = ref(null);
-const items_test = ref([]);
-
-function query_course(course_name) {
-  axios.get(USOS_API_URL + '/courses/search', {
+  await axios.get(USOS_API_URL + '/courses/search', {
       params: {
         lang: 'pl',
         name: course_name,
@@ -48,104 +38,110 @@ function query_course(course_name) {
       }
     })
     .then(function (response) {
-      console.log(response.data.items[0])
-      items_test.value = response.data.items.map((item) => { return {title: `${item.name.pl} (${item.course_id})`, value: item.course_id} } );
+      //console.log(response.data.items[0])
+      items_course.value = response.data.items.map((item) => { return {title: `${item.name.pl} (${item.course_id})`, value: item.course_id} } );
     })
     .catch(function (error) {
       console.log(error)
     })
     .finally(function () {
-      // always executed
+      loading_course.value = false
     })
-
-  items_test.value = [
-    { title: 'md', value: 5},
-  ]
 }
 
-const loading = ref(false)
-const items = ref([])
-const search = ref(null)
-const select = ref(null)
-const states = [
-  'Alabama',
-  'Alaska',
-  'American Samoa',
-  'Arizona',
-  'Arkansas',
-  'California',
-  'Colorado',
-  'Connecticut',
-  'Delaware',
-  'District of Columbia',
-  'Federated States of Micronesia',
-  'Florida',
-  'Georgia',
-  'Guam',
-  'Hawaii',
-  'Idaho',
-  'Illinois',
-  'Indiana',
-  'Iowa',
-  'Kansas',
-  'Kentucky',
-  'Louisiana',
-  'Maine',
-  'Marshall Islands',
-  'Maryland',
-  'Massachusetts',
-  'Michigan',
-  'Minnesota',
-  'Mississippi',
-  'Missouri',
-  'Montana',
-  'Nebraska',
-  'Nevada',
-  'New Hampshire',
-  'New Jersey',
-  'New Mexico',
-  'New York',
-  'North Carolina',
-  'North Dakota',
-  'Northern Mariana Islands',
-  'Ohio',
-  'Oklahoma',
-  'Oregon',
-  'Palau',
-  'Pennsylvania',
-  'Puerto Rico',
-  'Rhode Island',
-  'South Carolina',
-  'South Dakota',
-  'Tennessee',
-  'Texas',
-  'Utah',
-  'Vermont',
-  'Virgin Island',
-  'Virginia',
-  'Washington',
-  'West Virginia',
-  'Wisconsin',
-  'Wyoming',
-]
+// Load groups
+async function query_course(course_id) {
+  let course = null
 
-/*
-watch(search, val => {
-  if (val && val !== select.value) {
-    querySelections(val)
-  }
-})
-*/
+  console.log(select_semester.value)
 
-function querySelections(v) {
-  loading.value = true
-  // Simulated ajax query
-  setTimeout(() => {
-    items.value = states.filter(e => {
-      return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
+  await axios.get(USOS_API_URL + '/courses/course_edition', {
+      params: {
+        course_id: course_id,
+        term_id: select_semester.value,
+        fields: 'course_name|term_id|course_units_ids',
+      }
     })
-    loading.value = false
-  }, 500)
+    .then(function (response) {
+      console.log('got course data')
+      console.log(response.data)
+      course = response.data;
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+    .finally(function () {
+      return course
+    })
+}
+
+async function query_unit_groups_numbers(unit_id) {
+  let unit_groups_ids = null
+
+  await axios.get(USOS_API_URL + '/courses/course_unit', {
+      params: {
+        course_unit_id: course_unit_id,
+        term_id: select_semester.value.value,
+        fields: 'class_groups',
+      }
+    })
+    .then(function (response) {
+      console.log(response.data)
+      unit_groups_ids = response.data.class_groups.map(item => item.number);
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+    .finally(function () {
+    })
+
+    return unit_groups_ids
+}
+
+async function query_group_dates(unit_id, group_number) {
+  let group_dates = null
+
+  await axios.get(USOS_API_URL + '/tt/classgroup_dates2', {
+      params: {
+        course_unit_id: course_unit_id,
+        term_id: select_semester.value.value,
+        fields: 'class_groups',
+      }
+    })
+    .then(function (response) {
+      console.log(response.data)
+      group_dates = response.data;
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+    .finally(function () {
+    })
+
+    return group_dates
+}
+
+function query_unit_dates(unit_id) {
+  let unit = { id: unit_id }
+
+  let group_numbers = query_unit_groups_numbers(unit_id)
+  unit.groups = group_numbers.map(gn => { return { number: gn, dates: query_group_dates(unit_id, gn)}})
+
+  return unit
+}
+
+async function query_course_dates(course_id) {
+  let course = await query_course(course_id)
+  console.log(`course is null: ${course === null}`)
+  console.log('course:')
+  console.log(course)
+  course.units = course.course_units_ids.map(cu_id => query_unit_dates)
+  return course
+}
+
+function query(x) {
+  //console.log(x)
+  console.log(query_course_dates(x))
 }
 
 </script>
@@ -155,7 +151,7 @@ function querySelections(v) {
     <v-navigation-drawer v-model="drawer" 
       class="bg-deep-purple"
       theme="dark"
-      width="400"
+      width="500"
       permanent
     >
       <!--  -->
@@ -171,54 +167,31 @@ function querySelections(v) {
       </v-list-item>
 
       <v-list-item>
-        <v-select
-          v-model="select_course"
-          :hint="select_course ? `${select_course.state}, ${select_course.abbr}` : 'nazwa przedmiotu, kod przedmiotu'"
-          :items="items_course"
-          item-title="state"
-          item-value="abbr"
-          label="Wyszukaj przedmiot"
-          placeholder="Wyszukaj przedmiot"
-          persistent-hint
-          return-object
-          single-line
-        ></v-select>
-      </v-list-item>
-
-      <v-list-item>
         <v-autocomplete
-          v-model="select_test"
-          v-model:search="search_test"
-          :loading="loading_test"
-          :items="items_test"
+          v-model="select_course"
+          v-model:search="search_course"
+          :loading="loading_course"
+          :items="items_course"
           item-title="title"
           item-value="value"
           hide-no-data
           hide-details
           label="Wyszukaj przedmiot"
-          @keyup.native.enter="query_course(search_test)"
+          @keyup.native.enter="query_course_search(search_course)"
         ></v-autocomplete>
       </v-list-item>
 
       <v-list-item>
-        <v-autocomplete
-          v-model="select"
-          v-model:search="search"
-          :loading="loading"
-          :items="items"
-          hide-no-data
-          hide-details
-          label="What state are you from?"
-          @keyup.native.enter="querySelections(search)"
-        ></v-autocomplete>
+        <v-btn variant="outlined" @click="query(select_course)">
+          Button
+        </v-btn>
       </v-list-item>
-
     </v-list>
       
     </v-navigation-drawer>
 
     <v-app-bar absolute color="pink">
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer">+</v-app-bar-nav-icon>
 
       <v-toolbar-title>PlanDoskonały</v-toolbar-title>
     </v-app-bar>
@@ -231,7 +204,4 @@ function querySelections(v) {
 </template>
 
 <style scoped>
-.v-app-bar {
-  background-color: purple;
-}
 </style>
